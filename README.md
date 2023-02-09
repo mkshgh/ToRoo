b2aae110f4cb9a15d1ed58589c59d5c931e734c8
mkshgh <mukesh.ghimire@outlook.com>
2023-02-10 03:16:27 +0545

    Todo: Insert List UI/ Readme Update

--- README.md
+++ README.md
@@ -1,8 +1,44 @@
 # ToRoo
 Really Simple TODO CLI in RUST
 
-## Wow I started the Project
-Hello ToRoo
+
+### ToDo Application
+- add topic
+- add description list
+
+### View
+- topic
+
+### Search
+- topic 
+- Description
+- All
+
+### Delete
+- Topic
+- Description
+- All
+
+### Sections of App
+
+- Listed -> can be moved to Doing
+- Doing -> can be moved to Listed or Done
+- Done  -> can be moved to Doing 
+
+### Custom Keyboard Bindings
+- I -> Insert New to the list
+- J/K -> Move Task Up or Down
+- D -> Remove the Task
+- E -> Edit the Task
+- Double Esc -> Close the Application
+
+### Integrations
+- Sync to Google / Onedrive
+- Export/Import using csv or other formats
 
 ## References
-[Build Tools](https://visualstudio.microsoft.com/thank-you-downloading-visual-studio/?sku=BuildTools&rel=16>)
+- [Not Building for Windows](https://stackoverflow.com/questions/55603111/unable-to-compile-rust-hello-world-on-windows-linker-link-exe-not-found)
+- [Reding line from terminal](https://www.tutorialspoint.com/rust/rust_input_output.htm)
+- [Printing Individual Lines]()
+- [Time in Format: chrono](https://docs.rs/chrono/latest/chrono/#formatting-and-parsing)
+- [Time in Strftime Format](https://docs.rs/chrono/latest/chrono/format/strftime/)
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,52 @@
+use std::{io::{self, Write}};
+use chrono::{self, DateTime, Utc};
+const _DATE_FORMAT_STR: &'static str = "%Y-%m-%dT%H:%M:%S%.3fz";
+//time.format(DATE_FORMAT_STR).to_string()
 fn main() {
-    println!("Hello, ToRoo!");
+    let mstr = "List Name: has following tasks:";
+    println!("{}",mstr.len());
+    let (_time, list_name) = ask_for_list();
+    let task_vector = ask_tasks();
+    printTaskList(list_name,task_vector);
+}
+
+fn ask_for_list() -> (DateTime<Utc>,String){
+    let time_in_sec = chrono::offset::Utc::now();
+    print!("Insert a To-Do List: ");
+    io::stdout().flush();
+    let mut todo_list:String = String::from("");
+    std::io::stdin().read_line(&mut todo_list).unwrap();
+    (time_in_sec,todo_list)
+}
+fn ask_for_list_test() -> (DateTime<Utc>,String){
+    let time_in_sec = chrono::offset::Utc::now();
+    let mut todo_list:String = String::from("List 1");
+    (time_in_sec,todo_list)
+}
+fn ask_tasks()-> Vec<String>{
+    println!("Add Tasks to the lists (Leave Blank and Press Enter to Finish):");
+    let mut task_vector:Vec<String> = Vec::new();
+    loop{
+        print!("- ");
+        let mut myWorks = String::from("");
+        io::stdout().flush();
+        std::io::stdin().read_line(&mut myWorks).unwrap();
+        if myWorks=="\r\n"{
+            break;
+        }
+        else{
+            task_vector.push(myWorks.trim().to_owned());
+            let myWorks="";
+        }
+    }
+    task_vector
+}
+
+fn printTaskList(list_name:String,task_vector:Vec<String>){
+    println!("\nList Name:{} has following {} tasks:",list_name,task_vector.len().to_string());
+    let decorators = "-".repeat(35+task_vector.len());
+    println!("{}",decorators);
+    for tasks in &task_vector{
+        println!("-> {}",tasks);
+    }
 }
